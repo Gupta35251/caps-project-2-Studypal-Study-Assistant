8bdab43910fe2dec642563d6ecbd58b7dda0c46a
Prince Gupta <princegupta251201@gmail.com>
2026-06-26 00:47:02 +0530

    Updated file

--- vectorize_script.py
+++ vectorize_script.py
@@ -1,10 +1,10 @@
 from vectorize_book import vectorize_book_and_store_to_db,vectorize_individually
 
 
-# vectorize_book_and_store_to_db("Class 9/Science")
-# vectorize_book_and_store_to_db("Class 9/English")
-# vectorize_book_and_store_to_db("Class 9/Hindi")
+vectorize_book_and_store_to_db("Class 9/Science")
+vectorize_book_and_store_to_db("Class 9/English")
+vectorize_book_and_store_to_db("Class 9/Hindi")
 
-# vectorize_individually("Class 9/Science")
+vectorize_individually("Class 9/Science")
 vectorize_individually("Class 9/English")
 vectorize_individually("Class 9/Hindi")
